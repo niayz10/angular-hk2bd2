4cce918f7446729eb6430efb1f5be4455250dbf4
niayz10 <niayz10@mail.ru>
2020-02-14 03:23:14 +0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-hk2bd2
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-hk2bd2)
---
+++ src/app/products.ts
@@ -0,0 +1,91 @@
+export const products = [
+  {
+    id:"a1",
+    link:"https://aliexpress.ru/item/4000282435794.html?spm=a2g0o.tm75989.9778834960.1.16cf28dcbEwCqC&scm=1007.25281.150765.0&scm_id=1007.25281.150765.0&scm-url=1007.25281.150765.0&pvid=4390b018-1ed0-4072-890a-4088f015f843",
+    name: 'GAOMON PD2200 ',
+    price: 799,
+    images:["https://ae01.alicdn.com/kf/H0fc6dc355506464293aec4d903b90512K/GAOMON-PD2200-21-5-92-NTSC.jpg",
+    "https://ae01.alicdn.com/kf/Ud3ccecfc0d2141e493713c25e4c7e5a5M/GAOMON-PD2200-21-5-92-NTSC.jpg","https://ae01.alicdn.com/kf/U1c1e095a83444b3dbb98e10243efdb06g/GAOMON-PD2200-21-5-92-NTSC.jpg"],
+    description: '21,5 Дюймов 92% NTSC графический Рисунок планшет монитор ручка дисплей с поддержкой наклона без батареи 8192 ручка давление'
+  },
+  {
+    id:"a2",
+    link:"https://aliexpress.ru/item/4000193674517.html?spm=a2g0o.tm75989.9778834960.6.16cf28dcbEwCqC&scm=1007.25281.150765.0&scm_id=1007.25281.150765.0&scm-url=1007.25281.150765.0&pvid=4390b018-1ed0-4072-890a-4088f015f843",
+    name: 'Teclast P10HD',
+    images:["https://ae01.alicdn.com/kf/Hcd8440638d8c4a37b306ea4c09f91209s/Teclast-P10HD-10-1-Android-9-0-1920x1200-SC9863A-3.jpg",
+    "https://ae01.alicdn.com/kf/H12ddee92dacc426e95881283f7161e3bI/Teclast-P10HD-10-1-Android-9-0-1920x1200-SC9863A-3.jpg","https://ae01.alicdn.com/kf/H647c092ef8394b1ea6b022a02439b2dfH/Teclast-P10HD-10-1-Android-9-0-1920x1200-SC9863A-3.jpg"],
+    price: 699,
+    description: '10,1 "Android 9,0 планшет 1920x1200 SC9863A Восьмиядерный 3 ГБ ОЗУ 32 Гб ПЗУ 4G сеть AI скоростные планшеты ПК двойной Wifi'
+  },
+  {
+    id:"a3",
+    link:"https://aliexpress.ru/item/1000008052276.html?spm=a2g0o.tm75989.9778834960.3.16cf28dcbEwCqC&scm=1007.25281.150765.0&scm_id=1007.25281.150765.0&scm-url=1007.25281.150765.0&pvid=4390b018-1ed0-4072-890a-4088f015f843",
+    name: 'MaiBenBen XiaoMai 6A',
+    price: 23,
+    images:["https://ae01.alicdn.com/kf/HTB1gskHaV67gK0jSZPfq6yhhFXam.jpg","https://ae01.alicdn.com/kf/HTB1ijo1aRCw3KVjSZFlq6AJkFXaC.jpg","https://ae01.alicdn.com/kf/HTB129oVaQWE3KVjSZSyq6xocXXaf.jpg"],
+    description: 'для офисного ноутбука N4100 + MX250 2G графическая карта/8G ram/480G SSD + 1 ТБ HHD/DOS/серебристый металлический ноутбук 15,6 "ADS'
+  },
+  {
+    id:"a4",
+    link:"https://aliexpress.ru/item/32966918119.html?spm=a2g0o.tm75989.9778834960.5.16cf28dcbEwCqC&scm=1007.25281.150765.0&scm_id=1007.25281.150765.0&scm-url=1007.25281.150765.0&pvid=4390b018-1ed0-4072-890a-4088f015f843",
+    name: 'XCY Intel Core i5 7200U i3 7100U i7 4500U',
+    price: 23,
+    images:["https://ae01.alicdn.com/kf/HTB1gKu.cfWG3KVjSZFPq6xaiXXaf.jpg","https://ae01.alicdn.com/kf/HTB1HhDOazDuK1Rjy1zjq6zraFXap/XCY-Intel-Core-i5-7200U-i3-7100U-i7-4500U-Windows.jpg_50x50.jpg","https://ae01.alicdn.com/kf/HTB1RbpabGWs3KVjSZFxq6yWUXXab/XCY-Intel-Core-i5-7200U-i3-7100U-i7-4500U-Windows.jpg"],
+    description: ' безвентиляторный мини-ПК Windows 10 4K HTPC тонкий клиент настольный компьютер NUC HDMI VGA WiFi 6USB'
+  },
+   {
+     id:"a5",
+      link:"https://aliexpress.ru/item/4000193213484.html?spm=a2g0o.tm75989.9778834960.2.16cf28dcbEwCqC&scm=1007.25281.150765.0&scm_id=1007.25281.150765.0&scm-url=1007.25281.150765.0&pvid=4390b018-1ed0-4072-890a-4088f015f843",
+    name: 'Планшет Teclast T30 Andriod 9,0 4G',
+    price: 23,
+    images:["https://ae01.alicdn.com/kf/H04b56872de4b4e3b962aa33336864d5dB.jpg","https://ae01.alicdn.com/kf/Hf4eaf06f455444caa7df84d6b9559ed3T/Teclast-T30-Andriod-9-0-4G-10.jpg","https://ae01.alicdn.com/kf/Hcb17f7afb73e4bada2835b81267ff3dbk/Teclast-T30-Andriod-9-0-4G-10.jpg"],
+    description: 'с функцией телефонных звонков 10,1 "1920x1200 MTK P70 4 Гб ОЗУ 64 Гб ПЗУ 8000 мАч планшеты ПК Двойная камера gps type-C'
+   },
+   {
+     id:"a6",
+      link:"https://aliexpress.ru/item/4000269299395.html?spm=a2g0o.tm75989.9778834960.16.16cf28dcbEwCqC",
+    name: 'SDETER',
+    price: 23,
+    images:["https://ae01.alicdn.com/kf/Hadf7b9b52f01473699fe97d183766c0bK.jpg","https://ae01.alicdn.com/kf/Hb25f23e143534642acb6368352f0a0fcl/SDETER-HD-720P-DV.jpg","https://ae01.alicdn.com/kf/Hfcd82933a013454eb0d1eea92f240e030/SDETER-HD-720P-DV.png"],
+    description: 'с функцией телефонных звонков 10,1 "1920x1200 MTK P70 4 Гб ОЗУ 64 Гб ПЗУ 8000 мАч планшеты ПК Двойная камера gps type-C'
+   },
+   {
+     id:"a10",
+      link:"https://aliexpress.ru/item/32818601228.html?spm=a2g0o.tm75989.9778834960.14.16cf28dcbEwCqC",
+    name: 'KERUI M525',
+    price: 23,
+    images:["https://ae01.alicdn.com/kf/HTB1ISFdbGSs3KVjSZPiq6AsiVXaS.jpg","https://ae01.alicdn.com/kf/H7bd185533e64473cb5c50ca6c9a6208c8/KERUI-M525.jpg","https://ae01.alicdn.com/kf/HTB1ADThXBGE3KVjSZFhq6AkaFXa3/KERUI-M525.jpg"],
+    description: 'Главная Безопасность Добро пожаловать Беспроводной дверной звонок смарт-куранты дверной звонок сигнализации светодиодный свет 32 песни с водонепроницаемая сенсорная кнопка'
+   },
+   {
+     id:"a7",
+      link:"https://aliexpress.ru/item/4000069012590.html?spm=a2g0o.tm75989.9778834960.17.16cf28dcbEwCqC",
+    name: '1080P облако Wi-Fi PTZ ',
+    price: 23,
+    images:["https://ae01.alicdn.com/kf/H79673ed756e9459d856f696644592a04J.jpg","https://ae01.alicdn.com/kf/H5c0f1d9c49d3489db9be3782940eb501o/1080P-Wi-Fi-PTZ-2MP.jpg","https://ae01.alicdn.com/kf/He2697b4bae0a404f8fb630dbac4c5034w/1080P-Wi-Fi-PTZ-2MP.jpg"],
+    description: 'камера Открытый 2MP Авто слежение Домашняя безопасность ip-камера 4X цифровой зум скорость купольная камера с сиреной светильник'
+   },
+   {
+     id:"a8",
+      link:"https://aliexpress.ru/item/32385677608.html?spm=a2g0o.tm75989.9778834960.15.16cf28dcbEwCqC",
+      name:"Vstarcam 720П HD",
+    price: 23,
+    images:["https://ae01.alicdn.com/kf/HTB1YDYEX1KAUKJjSZFzq6xdQFXa7/Vstarcam-720-HD-WiFi-IP-Onvfi.jpg","https://ae01.alicdn.com/kf/HTB1YDYEX1KAUKJjSZFzq6xdQFXa7/Vstarcam-720-HD-WiFi-IP-Onvfi.jpg","https://ae01.alicdn.com/kf/HTB1hFrYkz7nBKNjSZLeq6zxCFXal/Vstarcam-720-HD-WiFi-IP-Onvfi.jpg"],
+    description: ' камера видеонаблюдения WiFi IP-камера Onvfi видеодомофон  Система ночного видения Мобильный удаленный просмотр в Монитор младенца'
+   },
+   {
+     id:"a9",
+      link:"https://aliexpress.ru/item/32225359554.html?spm=a2g0o.tm75989.9778834960.13.16cf28dcbEwCqC",
+    name: 'IOS Android приложение',
+    price: 23,
+    images:["https://ae01.alicdn.com/kf/He2f0877c5ffd4289893201a1168cfa14L/IOS-Android.jpg","https://ae01.alicdn.com/kf/HTB1KIYAg5CYBuNkHFCcq6AHtVXah/IOS-Android.jpg","https://ae01.alicdn.com/kf/H0a4da54ba1594f3b8a65ddd0c106d703q/IOS-Android.jpg"],
+    description: 'Проводная беспроводная домашняя безопасность LCD PSTN WIFI GSM сигнализация Система Интерком пульт дистанционного управления Автонабор датчиков сирены'
+   }
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
